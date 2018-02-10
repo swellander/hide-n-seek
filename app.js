cd7fd4614313dcd5d6459a2f95335e9335185809
Sam Wellander <samwellander@Sams-MacBook-Pro.local>
2018-02-10 09:54:30 -0800

    add ejs and jquery

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('HEYYYYYYY');
-})
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+  })
 
 app.listen(3000);
 console.log('Server started on port 3000');
