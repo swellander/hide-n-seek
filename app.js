e4ec48b63099485512c54b6d5eb5eb144dc3ff22
Sam Wellander <samwellander@Sams-MacBook-Pro.local>
2018-02-12 11:11:31 -0800

    Add QR route logic

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var qr = require('qr-image');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('index.ejs');
-  })
+  });
+
+app.post('/', function(req, res) {
+
+  //set form input data to variables
+  var url = req.body.qr_url;
+  var imgName = req.body.qr_name;
+
+  var code = qr.image(url, { type: 'png' });
+  res.setHeader('Content-type', 'image/png');  //sent qr image to client side
+  code.pipe(res);
+});
+
 
 app.listen(3000);
 console.log('Server started on port 3000');
